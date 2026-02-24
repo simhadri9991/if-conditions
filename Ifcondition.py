f1682806640f9df90b5fb28eec588e76a000ca48
simhadri9991 <simhadribhupani4@gmail.com>
2026-02-24 20:02:29 +0530

    Add files via upload

--- Ifcondition.py
+++ Ifcondition.py
@@ -176,4 +176,268 @@ if not a:
 b=int(input("value"))
 if b in a:
     print("the value is",b)'''
+#if-else conditions by using comparision operators
+'''a=6
+b=8
+if a<b:
+    print("less")'''
+
+'''a=6
+b=8
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+'''a=6
+b=8
+if a<b:
+    print("less")
+else:
+    print("false")'''
+
+
+'''a=3
+b=9
+if a!=b:
+    print("not equal")
+else:
+    print("equal")'''
+
+
+'''a=9
+b=3
+if a==b:
+    print("not equal")
+else:
+    print("equal")'''
+#runtime if else
+
+'''a=int(input())
+b=int(input())
+if a>b:
+    print("trule")
+else:
+    print("false")'''
+#if-else conditions by using logical operators
+'''a=6
+b=12
+if a<b and b>a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=3
+b=7
+if a!=b or b==a:
+    print("equal")
+else:
+    print("not equal")'''
+
+'''a=9
+b=10
+if not b>a:
+    print("false")
+else:
+    print("true")'''
+
+'''a=int(input())
+b=int(input())
+if a>b or b<a:
+    print("true")
+else:
+    print("false")'''
+#if-else by identity operators
+#is, is not
+#string
+'''a="python"
+if type(a) is str:
+    print("yes")
+else:
+    print("no")'''
+
+'''a="python"
+if type(a) is not str:
+    print("yes")
+else:
+    print("no")'''
+#int
+'''a=22
+if type(a) is int:
+    print("yes")
+else:
+    print("no")'''
+
+'''a=22
+if type(a) is not int:
+    print("yes")
+else:
+    print("no")'''
+#float
+'''a=2.2
+if type(a) is int:
+    print("yes")
+else:
+    print("no")'''
+
+'''a=2.2
+if type(a) is not int:
+    print("yes")
+else:
+    print("no")'''
+#complex
+'''a=2
+if type(a) is complex:
+    print("yes")
+else:
+    print("no")'''
+
+'''a=2+6j
+if type(a) is not int:
+    print("yes")
+else:
+    print("no")'''
+#boolean
+'''a=True
+if type(a) is bool:
+    print("yes")
+else:
+    print("no")'''
+
+'''a=False
+if type(a) is not bool:
+    print("yes")
+else:
+    print("no")'''
+#if-else conditions by using membership operators
+#in,not in
+'''a=[1,2,3,4,5,6,7]
+if 5 in a:
+    print("Value is there")
+else:
+    print("value is not there")'''
+
+'''a=[1,2,3,4,5,6,7]
+if not 5 in a:
+    print("Value is there")
+else:
+    print("value is not there")'''
+
+#runtime
+'''a=[1,2,3,4,5,6,7]
+b=int(input("a value is"))
+if b in a:
+    print("the value is",b)
+else:
+    print("false")'''
+      
+'''a=[1,2,3,4,5,6,7]
+b=int(input("a value is"))
+if not b in a:
+    print("the value is",b)
+else:
+    print("false")'''
+#if-elif-else
+'''a=8
+b=12
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+    
+'''a=8
+b=12
+if a>b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=8
+b=12
+if a>b:
+    print("less")
+elif b==a:
+    print("greater")
+else:
+    print("true")'''
+
+'''a=8
+b=12
+if a>b:
+    print("less")
+elif b==a:
+    print("greater")
+elif a<b:
+    print("false")
+else:
+    print("true")'''
+#logical operators
+#and, or, not
+'''a=8
+b=12
+if b>a and a>b:
+    print("less")
+elif b==a or a!=b:
+    print("greater")
+elif not a<b:
+    print("false")
+else:
+    print("true")'''
+
+'''a=10
+b=20
+if a>b and a==b:
+    print("greater")
+elif a>=b or b>a:
+    print("lesser")
+elif not a!=b:
+    print("greater")
+else:
+    print("lesser")'''
+#membership operators
+'''a=[12,20,30,40,50]
+if 50 in a:
+    print("greater")
+elif 20 not in a:
+    print("true")
+else:
+    print("lesser")'''
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
